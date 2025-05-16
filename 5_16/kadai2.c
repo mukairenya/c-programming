6666cc34695ce80020ab167bc9bfaee4196aa199
mukairenya <mch-210-pwk@e-net.nara.jp>
2025-05-16 06:22:06 +0000

    5_16

---
+++ 5_16/kadai2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int tasizan(int a,int b);
+int tasizan(int a,int b);
+int kakeru(int a,int b);
+int main(void){
+    int a,b,i;
+    scanf("%d\n",&i);
+    scanf("%d\n",&a);
+    scanf("%d\n",&b);
+printf("%d\n",a+b);
+printf("%d\n",a-b);
+printf("%d\n",a*b);
+    return 0;
+}
+if(d==0)
+printf("足し算を選択しました\n");
+int tasizan(int a,int b){
+    return a+b;
+}
+else if(d==1){
+printf("引き算を選択しました\n");
+int hiku(int a,int b){
+int goukei;
+goukei=a-b;
+}
+return a-b;
+}
+else(d==2)
+printf("掛け算を選択しました\n");
+int kakeru(int a,int b){
+int goukei;
+goukei=a*b;
+return a*b;
+}
